08d3bffff6a32b7edd751561683941a83c310ee4
wangbin <2722620236@qq.com>
2022-07-25 14:38:43 +0800

    修改clang-fromat脚本

--- clang-format.sh
+++ clang-format.sh
@@ -1,2 +1,8 @@
 #find . -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec echo  {} \;
+
+if [ $(dpkg -l |grep clang-format-7 | wc -l) -eq 0 ];then
+	sudo apt install clang-format-7
+fi
+
+
 find . -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format-7 -i {} \;
